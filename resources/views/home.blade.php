234add3f92871c73d4b24af055e88954017e1126
mdeias <mdeias@icloud.com>
2022-01-24 16:33:29 +0100

    stampa lista nella home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,27 @@
 @extends('layouts.app')
 @section('content')
-    
+<main>
+    <h1>Lista fumetti</h1>
+    <div class="contenitore">
+
+        @foreach ($comics as $comic )
+            <div class="card">
+                <div class="copertina">
+
+                    <img src="{{ $comic['thumb'] }}" alt="{{ $comic['title'] }}">
+                    
+                </div>
+
+                <p>{{ $comic['title'] }}</p>
+
+            </div>
+        @endforeach
+
+
+
+
+
+    </div>
+
+  </main>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+
+    $data = config('comics');
+
+    return view('home', ['comics' => $data]);
 })->name('home');
 
 
